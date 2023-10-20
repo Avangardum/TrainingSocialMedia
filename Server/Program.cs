6179289daf9865eb7311412b941189c431a6cb73
Avangardum <yurykuz65@gmail.com>
2023-10-20 17:22:59 +0300

    Revert "Added an exception on the server startup"
    This reverts commit 5bae2e4af9302d5acb0d90949db84901636f28f8.

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.ResponseCompression;
 
-throw new Exception("Oh no! An error occurred! :(");
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
