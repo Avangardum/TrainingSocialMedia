5134fbaca36c29f952aea13910aaf51d6e5c49d2
Avangardum <yurykuz65@gmail.com>
2023-10-23 19:01:39 +0300

    Configured EfCore

---
+++ Server/Data/TrainingSocialMediaDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TrainingSocialMedia.Server.Data;
+
+public class TrainingSocialMediaDbContext : DbContext
+{
+    public TrainingSocialMediaDbContext(DbContextOptions<TrainingSocialMediaDbContext> options) : base(options) { }
+}
--- Server/Program.cs
+++ Server/Program.cs
@@ -1,36 +1,62 @@
-using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.EntityFrameworkCore;
+using TrainingSocialMedia.Server.Data;
+
+
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllersWithViews();
-builder.Services.AddRazorPages();
-
+ConfigureServices();
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseWebAssemblyDebugging();
-}
-else
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-
-app.UseBlazorFrameworkFiles();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-
-app.MapRazorPages();
-app.MapControllers();
-app.MapFallbackToFile("index.html");
-
+MigrateDb();
+ConfigurePipeline();
 app.Run();
+
+
+
+void ConfigureServices()
+{
+    builder.Services.AddControllersWithViews();
+    builder.Services.AddRazorPages();
+
+    var dbPassword = builder.Configuration["DB_PASSWORD"];
+    if (string.IsNullOrEmpty(dbPassword))
+        throw new Exception("DB password is not set");
+    var dbConnectionString = $"server=db;uid=root;pwd={dbPassword};database=TrainingSocialMedia";
+    builder.Services.AddDbContext<TrainingSocialMediaDbContext>(options =>
+    {
+        options.UseMySql(dbConnectionString, ServerVersion.AutoDetect(dbConnectionString), mySqlOptions =>
+        {
+            mySqlOptions.CommandTimeout(10);
+        });
+    });
+}
+
+void ConfigurePipeline()
+{
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseWebAssemblyDebugging();
+    }
+    else
+    {
+        app.UseExceptionHandler("/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseBlazorFrameworkFiles();
+    app.UseStaticFiles();
+
+    app.UseRouting();
+
+    app.MapRazorPages();
+    app.MapControllers();
+    app.MapFallbackToFile("index.html");
+}
+
+void MigrateDb()
+{
+    using var dbContext = app.Services.GetRequiredService<TrainingSocialMediaDbContext>();
+    dbContext.Database.Migrate();
+}
