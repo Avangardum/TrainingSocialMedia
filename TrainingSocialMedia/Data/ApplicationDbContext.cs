47edee4a96a80bdc387896bc532ad9e26942532e
Avangardum <yurykuz65@gmail.com>
2023-10-27 13:26:01 +0300

    Added a new Blazor Server project

---
+++ TrainingSocialMedia/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace TrainingSocialMedia.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
